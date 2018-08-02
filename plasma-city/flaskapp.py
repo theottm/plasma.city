621af9e601435a8fb024bc0a7b911c1473bdfc71
Theo Tyburn <theo.tyburn@plasma.city>
2018-08-02 17:03:32 +0200

    following the gandi instructions for python servers

---
+++ plasma-city/flaskapp.py
@@ -0,0 +1,52 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+from flask import url_for
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/usr/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/codex/')
+def codex():
+    return render_template('codex.html')
+
+@app.route('/hello-ob/')
+def hello_ob():
+    return render_template('hello-ob.html')
